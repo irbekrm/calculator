3b5a5cfb8e25786036aae8b4386444fb0eee1c85
irbekrm <irbekrm@gmail.com>
2017-11-06 18:18:53 +0100

    basic layout

--- src/Screen.js
+++ src/Screen.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Screen = props => (<div class="screen">
-  <p>{"M: " + props.memory}</p>
-  <p>{props.currentChar}</p>
-  <p>{props.currentSequence}</p>
+  <span>{"M: " + props.memory}</span>
+  <span>{props.currentChar}</span>
+  <span>{props.currentSequence}</span>
 </div>);
 
 export default Screen;
