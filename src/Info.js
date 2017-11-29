1b8e9c84af0dd109e7866ba34a6781102e37e4c0
irbekrm <irbekrm@gmail.com>
2017-11-29 11:28:31 +0100

    Clearer textual description

--- src/Info.js
+++ src/Info.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 const Info = _ => (
-  <div id="innerInfo"><p>Allows to use numbers up to 9 digits long (displays longer calculation
-  results in exponential form).</p>
-  <p><b>C</b> - clear the last char.</p>
-  <p><b>CA</b> - clear the display and current calculation sequence</p>
-  <p><b>M</b> - add currently displayed number to memory</p>
-  <p><b>M+</b> - add currently displayed number to the number stored in memory</p>
-  <p><b>M-</b> - subtract the currently displayed number from the number stored
+  <div id="innerInfo"><p>Perform calculations with numbers up to 9 digits long
+   (If the result of a calculation is longer than 9 digits, it will be
+   displayed in an exponential form)</p>
+  <p><b>C</b> - clear the last character or in an ongoing calculation sequence
+  or clear the displayed value and calculation sequence if the sequence
+  has already been evaluated</p>
+  <p><b>CA</b> - clear the display and the current calculation sequence</p>
+  <p><b>M</b> - replace the value stored in memory by the currently displayed
+   value. Memory value is 0 by default</p>
+  <p><b>M+</b> - add currently the displayed value to the value stored in memory</p>
+  <p><b>M-</b> - subtract the currently displayed value from the value stored
   in memory</p>
   <p>Attempt to divide by zero will result in an error</p>
   </div>);
